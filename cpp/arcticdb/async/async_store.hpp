58d39a76524cd4472dc04667862f359c7adc7d71
William Dealtry <william.dealtry@gmail.com>
2024-10-28 13:31:38 +0000

    Add some lightweight encodings

--- cpp/arcticdb/async/async_store.hpp
+++ cpp/arcticdb/async/async_store.hpp
@@ -51,11 +51,11 @@ class AsyncStore : public Store {
 public:
     AsyncStore(
         std::shared_ptr<storage::Library> library,
-        const arcticdb::proto::encoding::VariantCodec &codec,
+        const BlockCodecImpl&& codec,
         EncodingVersion encoding_version
     ) :
         library_(std::move(library)),
-        codec_(std::make_shared<arcticdb::proto::encoding::VariantCodec>(codec)),
+        codec_(std::make_shared<BlockCodecImpl>(codec)),
         encoding_version_(encoding_version) {
     }
 
@@ -395,7 +395,7 @@ folly::Future<SliceAndKey> async_write(
 private:
     friend class arcticdb::toolbox::apy::LibraryTool;
     std::shared_ptr<storage::Library> library_;
-    std::shared_ptr<arcticdb::proto::encoding::VariantCodec> codec_;
+    std::shared_ptr<BlockCodecImpl> codec_;
     const EncodingVersion encoding_version_;
 };
 
